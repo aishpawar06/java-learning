110f5d2385c865068d41176cad36486b695cf733
Aishwarya Pawar <aishwarya06.pawar@gmail.com>
2026-01-25 21:01:21 +0530

    Add Java basics structure and variables example

---
+++ 01-basics/Variables.java
@@ -0,0 +1,10 @@
+public class Variables {
+    public static void main(String args[]){
+        int var1 = 10;
+        int var2 = 20;
+        String var3 = "Tony";
+        System.out.println("var1 = "+var1);
+        System.out.println("var2 = "+var2);
+        System.out.println("var3 = "var3);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # java-learning
 java concepts 
+This repository contains my Java learning journey.
+I am learning software development concepts and pushing code regularly.
+
+## Topics
+- Java Basics
+- Conditionals
+- Loops
+- Functions
+- Arrays
+- OOP
