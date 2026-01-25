bb3412065b14bdbc2e0770849a3977e9433803f8
Aishwarya Pawar <aishwarya06.pawar@gmail.com>
2026-01-25 22:15:08 +0530

    add all datatypes

---
+++ 01-basics/DataTypes.java
@@ -0,0 +1,20 @@
+public class DataTypes {
+    public static void main(String args[]){
+        byte b = 8; //range is upto -128 to +127
+        System.out.println("byte = "+b);
+        boolean var = true;
+        System.out.println("Boolean = "+var);
+        float price = 10.1f;
+        System.out.println("Float = "+price);
+        int number = 10;
+        System.out.println("integer = "+number);
+        //long is used for big integers
+        long l = 10000000;
+        System.out.println("long = "+l);
+        //double is used for high precision floating point numbers
+        double d = 3.1414232423;
+        System.out.println("Double = "+ d);
+        short s = 1000;
+        System.out.println("Short = "+s);
+    }
+}
