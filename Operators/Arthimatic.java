3e8e0325407570bf1e2b32cd4dd10593cd533265
Aishwarya Pawar <aishwarya06.pawar@gmail.com>
2026-01-29 19:31:01 +0530

    Adding unary operators

--- Operators/Arthimatic.java
+++ Operators/Arthimatic.java
@@ -12,5 +12,18 @@ public class Arthimatic {
         System.out.println("Multiplication is : "+(a*b));
         System.out.println("Division is : "+(a/b));
         System.out.println("Modulo is : "+(a%b));
+
+        // Unary operators
+        System.out.println("\nPre increment\n");
+        int pre1 = 10;
+        int pre2 = ++pre1;
+        System.out.println(pre1);
+        System.out.println(pre2);
+
+        System.out.println("\nPost increment\n");
+        int post1 = 10;
+        int post2 = post1++;
+        System.out.println(post1);
+        System.out.println(post2);
     }
 }
