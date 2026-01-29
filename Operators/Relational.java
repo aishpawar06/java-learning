713f13031d12ede5f0782ffb3d690f84f5ab6eeb
Aishwarya Pawar <aishwarya06.pawar@gmail.com>
2026-01-29 20:17:41 +0530

    Testing relational operators

---
+++ Operators/Relational.java
@@ -0,0 +1,36 @@
+package Operators;
+
+public class Relational {
+    public static void main(String args[]){
+        
+        // == 
+        System.out.println("Equity ");
+        System.out.println( 4 == 4 ); 
+        System.out.println( 4 == 5 );
+        
+        // < Less than
+        System.out.println("\nLess than");
+        System.out.println( 4 < 5);
+        System.out.println( 5 < 2 );
+        
+        // > More than
+        System.out.println("\nmore than");
+        System.out.println( 5 > 8);
+        System.out.println( 5 > 2);
+        
+        // <= Less than equal to
+        System.out.println( "\nLess than equal to ");
+        System.out.println( 5 <= 5 );
+        System.out.println( 5 <= 2);
+        
+        // <= more than equal to 
+        System.out.println( "\nMore than equal to ");
+        System.out.println( 5 >= 5 );
+        System.out.println( 5 >= 2);
+
+        // Not equal to 
+        System.out.println("\nNot equal to  ");
+        System.out.println( 4 != 4 ); 
+        System.out.println( 4 != 5 );
+    }
+}
