0818fb437654db568cbd55147e78c688f3552a70
Aishwarya Pawar <aishwarya06.pawar@gmail.com>
2026-01-31 20:50:20 +0530

    testing else if condition

---
+++ ConditionalStatements/else_if.java
@@ -0,0 +1,19 @@
+package ConditionalStatements;
+import java.util.Scanner;
+public class else_if {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your age: ");
+        int age = sc.nextInt();
+        if(age<0){
+            System.out.println("Invalid age!!");
+        }else if(age >=18){
+            System.out.println("Adult.");
+        }else if(age >=13 && age < 18){
+            System.out.println("Teenager.");
+        }else{
+            System.out.println("Child.");
+        }sc.close();
+    }
+    
+}
