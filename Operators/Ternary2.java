69448a584b78a9e48123fc01e454f7dd0dea2cf1
Aishwarya Pawar <aishwarya06.pawar@gmail.com>
2026-02-02 20:02:05 +0530

    Checking pass or fail using conditional operator

---
+++ Operators/Ternary2.java
@@ -0,0 +1,13 @@
+package Operators;
+import java.util.*;
+public class Ternary2 {
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your marks:");
+        int Marks = sc.nextInt();
+        System.out.println((Marks>=33)?"Pass":"Fail");
+    }
+
+    
+}
