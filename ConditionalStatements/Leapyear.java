28610824a47dc03f4ded79f86df4ec508edb07e9
Aishwarya Pawar <aishwarya06.pawar@gmail.com>
2026-02-02 20:05:00 +0530

    checking if a year is leap year or not using nested if statements

---
+++ ConditionalStatements/Leapyear.java
@@ -0,0 +1,23 @@
+package ConditionalStatements;
+import java.util.*;
+public class Leapyear {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter year: ");
+        int year = sc.nextInt();
+        if(year%4==0){
+            if(year%100==0){
+                if(year%400==0){
+                    System.out.println("Leap Year");
+                }else{
+                    System.out.println("Not a leap year");
+                }
+            }else{
+                System.out.println("leap year");
+            }
+        }else{
+            System.out.println("Not a leap year");
+        }
+    }
+    
+}
