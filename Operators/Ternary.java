0b4156e0f7be1ccef2a5a734e733c20bee7e4d68
Aishwarya Pawar <aishwarya06.pawar@gmail.com>
2026-02-02 20:01:22 +0530

    Checking even odd using conditional operator

---
+++ Operators/Ternary.java
@@ -0,0 +1,11 @@
+package Operators;
+import java.util.*;
+public class Ternary {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int num = sc.nextInt();
+        System.out.println((num%2==0)?"Even":"Odd");
+    }
+    
+}
