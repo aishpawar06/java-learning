4953d45e8c95cc91e779f150dea22ddb16cb22d6
Aishwarya Pawar <aishwarya06.pawar@gmail.com>
2026-02-03 23:39:48 +0530

    A simple square pattern

---
+++ Patterns/square.java
@@ -0,0 +1,11 @@
+package Patterns;
+
+public class square {
+    public static void main(String args[]){
+        int i = 1 ;
+        while(i<=5){
+            System.out.println("*****");
+            i++;
+        }
+    }
+}
