da38c07521fcb6bdddfe086e857490ebbfee3a2e
Aishwarya Pawar <aishwarya06.pawar@gmail.com>
2026-02-04 22:02:00 +0530

    Testing of do while loop

---
+++ loops/Dowhile.java
@@ -0,0 +1,10 @@
+package loops;
+public class Dowhile {
+    public static void main(String args[]){
+        int counter = 1;
+        do{
+            System.out.println("Hello World!");
+            counter++;
+        }while(counter  <= 10);
+    }
+}
