df951881c705321bc91958462768368a00c75416
Aishwarya Pawar <aishwarya06.pawar@gmail.com>
2026-02-07 12:00:31 +0530

    Print a number staircase

---
+++ Patterns/NumberStaircase.java
@@ -0,0 +1,12 @@
+package Patterns;
+public class NumberStaircase{
+    public static void main(String args[]){
+        int temp =1;
+        for(int i =1; i<=5;i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(temp+" ");
+                temp++;
+            }System.out.println();
+        }
+    }
+}
