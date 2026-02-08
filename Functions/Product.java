399be32335a7742e2067087d39deee4ab2f4abab
Aishwarya Pawar <aishwarya06.pawar@gmail.com>
2026-02-08 13:57:51 +0530

    Find the product of 2 numbers using functions

---
+++ Functions/Product.java
@@ -0,0 +1,14 @@
+//Find the product of 2 numbers using function 
+package Functions;
+
+public class Product {
+    public static int multiply(int a, int b){
+        return a*b;
+    }
+    public static void main(String args[]){
+        int a=5;
+        int b=10;
+        System.out.println(a + " x "+ b + " = " + multiply(a,b));
+
+    } 
+}
