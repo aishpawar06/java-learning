e23cddd622a80eac6603f653a6e0a8a034e3e1f6
Aishwarya Pawar <aishwarya06.pawar@gmail.com>
2026-02-08 14:19:41 +0530

    Find the Binomial Coeffient using a function

---
+++ Functions/BinomialCoeff.java
@@ -0,0 +1,26 @@
+//Find the Binomial Coefficient
+
+package Functions;
+
+public class BinomialCoeff {
+    public static int factorial(int n){
+        int fact=1;
+        if(n==0){
+            return 1;
+        }
+        else{
+            while(n>0){
+                fact*=n;
+                n--;
+            }
+        }
+        return fact;
+    }
+    public static int Binomial(int n, int r){
+        int BinCoeff = (factorial(n)/(factorial(r)*factorial(n-r)));
+        return BinCoeff;
+    }
+    public static void main(String args[]){
+        System.out.println(Binomial(10,2));
+    }
+}
