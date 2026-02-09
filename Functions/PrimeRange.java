3d0659204063c4aff16f08f18fb4c1f129c3575b
Aishwarya Pawar <aishwarya06.pawar@gmail.com>
2026-02-09 20:07:54 +0530

    Find out all the Prime numbers till a specific number

---
+++ Functions/PrimeRange.java
@@ -0,0 +1,23 @@
+//Find out all the prime numbers till n
+package Functions;
+
+public class PrimeRange {
+    public static void Prime(int n){
+        for(int i = 2; i<=n; i++){
+            boolean isPrime=true;
+            for(int j = 2;j<i;j++){
+                if(i%j==0){
+                    isPrime=false;
+                    break;
+                }
+            }
+            if(isPrime){
+                System.out.println(i);
+            }
+
+        }
+    }
+    public static void main(String args[]){
+        Prime(10);
+    }
+}
