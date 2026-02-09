ffdb670da06b88120663cfa138988f66d23b5e1b
Aishwarya Pawar <aishwarya06.pawar@gmail.com>
2026-02-09 19:59:52 +0530

    Optimzing the condition for primecheck

--- Functions/PrimeCheck.java
+++ Functions/PrimeCheck.java
@@ -1,10 +1,11 @@
 //Check for prime number using a build-in function
+
 package Functions;
 import java.util.Scanner;
 public class PrimeCheck{
 
     public static boolean prime(int n){
-        for(int i =2; i<=(n-1); i++){
+        for(int i =2; i<=Math.sqrt(n); i++){
             if(n%i==0){
                 return false;
             }
