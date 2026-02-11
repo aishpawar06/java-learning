116e83731b397592136d23b4eaada28683487f94
Aishwarya Pawar <aishwarya06.pawar@gmail.com>
2026-02-11 23:36:39 +0530

    Create a hollow rectangle pattern using stars

---
+++ Patterns/Hollowrect.java
@@ -0,0 +1,16 @@
+package Patterns;
+
+public class Hollowrect {
+    public static void main(String args[]){
+        int r=4,c=5;
+        for(int i =0; i<r;i++){
+            for(int j=0; j<c;j++){
+                if(i==0||j==0||i==(r-1)||j==(c-1)){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }System.out.println();
+        }
+    }
+}
