600ac08e445c00235b527597c1377f04934187ee
Aishwarya Pawar <aishwarya06.pawar@gmail.com>
2026-02-15 22:56:58 +0530

    Create a function to calculate the average of 3 integer numbers

---
+++ Functions/Avg.java
@@ -0,0 +1,17 @@
+package Functions;
+import java.util.Scanner;
+public class Avg {
+    public static int avg(int a, int b, int c){
+        int avg=((a+b+c)/3);
+        return avg;
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 3 numbers:");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        System.out.println("Average is : "+avg(a,b,c));
+        sc.close();
+    }
+}
