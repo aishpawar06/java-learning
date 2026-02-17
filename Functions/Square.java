6359487527007bb7f7ca958802bc846120fd67a5
Aishwarya Pawar <aishwarya06.pawar@gmail.com>
2026-02-17 23:25:11 +0530

    Calculate square

---
+++ Functions/Square.java
@@ -0,0 +1,13 @@
+package Functions;
+import java.util.Scanner;
+public class Square {
+    static int square(int n) {
+        return n * n;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(square(n));
+    }
+}
