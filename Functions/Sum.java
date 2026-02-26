b7862b5eff614e3efbd545f80f30b21b93775659
Aishwarya Pawar <aishwarya06.pawar@gmail.com>
2026-02-26 23:41:34 +0530

    function that adds the digits of an integer

---
+++ Functions/Sum.java
@@ -0,0 +1,20 @@
+// Write a function to calculate the sum of digits of an integer
+package Functions;
+import java.util.Scanner;
+public class Sum {
+    public static int add(int num){
+        int sum=0;
+        while(num>0){
+            sum += num%10;
+            num/=10;
+        }
+        return sum;
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num=sc.nextInt();
+        System.out.println("Sum is : "+add(num));
+    }
+    
+}
