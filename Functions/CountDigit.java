eedc149adeccbbd67d211346d0bb2ec2006b8269
Aishwarya Pawar <aishwarya06.pawar@gmail.com>
2026-03-06 23:21:48 +0530

    Function returns number of digits in a number

---
+++ Functions/CountDigit.java
@@ -0,0 +1,20 @@
+//Write a function that calculates the number of digits in a number
+package Functions;
+import java.util.Scanner;
+public class CountDigit {
+    public static int count(int num){
+        int count=0;
+        while(num>0){
+            num/=10;
+            count++;
+        }
+        return count;
+    }
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        System.out.println("Count: "+ count(num));
+    }
+    
+}
